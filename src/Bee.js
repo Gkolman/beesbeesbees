d5d30c4e8dc8fc99785251598227c358054b2f88
ryhorowitz <ryan.w.horowitz@gmail.com>
2020-11-02 22:52:49 -0500

    Complete Grub

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,16 @@
 var Bee = function() {
+  Grub.prototype
+  grub.prototype = {} // grub functions 
+  _.extends(Grub.prototype, Bee.prototype) 
 };
 
+
+// Create a Bee class, in pseudoclassical style, with:
+// call the Grub superclass
+// set the prototype
+// set the constructor
+// an age property that is set to 5
+// a color property that is set to yellow
+// a food property that is inherited from grub
+// an eat method that is inherited from grub
+// a job property that is set to keep on growing
